bb010410161e5e99d0ac9319a207dd54b844c409
rafal <figlus@student.agh.edu.pl>
2020-05-06 10:28:51 +0200

    zadania z srody

---
+++ zadania_praktyczne_SRODA/zadanie_01.py
@@ -0,0 +1,6 @@
+import request
+
+
+data = request.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+with open("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", 'w') as csv:
+    csv.write(data.text)
